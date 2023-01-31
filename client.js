f3ef0bf9cfb734d07a6a7e536673e71ec5369166
Dave Estey <daveestey.working@gmail.com>
2023-01-30 23:24:12 -0400

    Figured out I was trying to do two different things and went down a loop of two different directions, switched everything over to mySQL2, instead of express. Still working on transition.

--- client.js
+++ client.js
@@ -1,11 +1,11 @@
-
+const express = require("express");
 const inquirer = require("inquirer");
 const sql = require("mysql2");
 const table = require("console.table");
+const request = require("request");
 
 
-
-
+const app = express()
 const PORT = process.env.PORT || 3001
 
 
@@ -108,3 +108,6 @@ class Business {
 }
 
     // addRole() {}
+
+
+
