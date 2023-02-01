1af79da213a8ab527f0d32ae3d4b668e5c203e04
Dave Estey <daveestey.working@gmail.com>
2023-02-01 00:05:42 -0400

    fixed the inquirer problem and added more cases

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const inquirer = require("inquirer");
 const sql = require("mysql2");
 const table = require("console.table");
@@ -12,92 +11,87 @@ const db = sql.createConnection(
   }
 )
 
-db.connect(err => {
-  if (err) throw err;
-  console.log(`connected as id ${db.threadId}`);
-  const b = new Business;
-  b.makeMenu();
-});
-
-class Business {
-
-  constructor() { }
-
-  makeMenu() {
-    console.log("made menu")
-    return inquirer
-      .prompt(
-        {
-          type: "list",
-          name: "menu",
-          message: "Choose what you would like to do!",
-          choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
-        }
-      )
-      .then(data => {
-        switch (data.menu) {
-          case "View all departments":
-            this.viewDepartments();
-            break;
-          case "View all roles":
-            this.viewRoles();
-            break;
-          case "View all employees":
-            this.viewEmployees();
-            break;
-          case "Add a department":
-            this.addDeparnments();
-            break;
-          case "Add a role":
-            this.addRole();
-            break;
-          case "add an employee":
-            this.addEmployee();
-            break;
-          case "Update an employee":
-            this.updateEmployee();
-            break;
-        }
-        this.makeMenu();
-      });
-
-  }
 
 
 
 
 
-  viewDepartments() {
-    const getDepartments = `SELECT * FROM department`;
-    db.query(getDepartments, (err, data) => {
-      err ? res.status(500).json({ Error: err.message }) : console.table("\n", data);
+const makeMenu = () => {
+  inquirer
+    .prompt(
+      {
+        type: "list",
+        name: "menu",
+        message: "Choose what you would like to do!",
+        choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
+      }
+    )
+    .then(data => {
+      switch (data.menu) {
+        case "View all departments":
+          viewDepartments();
+          break;
+        case "View all roles":
+          viewRoles();
+          break;
+        case "View all employees":
+          viewEmployees();
+          break;
+        /* case "Add a department":
+          addDeparnments();
+        case "Add a role":
+          addRole();
+        case "add an employee":
+          addEmployee();
+        case "Update an employee":
+          updateEmployee(); */
+      }
     });
-  }
+}
 
-  viewRoles() {
-    const getRoles = `SELECT role.title AS Title, department.name AS Department, role.salary AS Salary
+
+makeMenu();
+
+
+const viewDepartments = () => {
+  const getDepartments = `SELECT * FROM department`;
+  db.query(getDepartments, (err, data) => {
+    if (err) throw err
+    else {
+      console.table(data);
+      makeMenu();
+    }
+  });
+}
+
+const viewRoles = () => {
+  const getRoles = `SELECT role.title AS Title, department.name AS Department, role.salary AS Salary
         FROM role
         LEFT JOIN department
           ON role.department_id = department.id`;
-    db.query(getRoles, (err, data) => {
-      if (err) throw err ;
-      else console.table(data);
-    });
-  }
+  db.query(getRoles, (err, data) => {
+    if (err) throw err
+    else {
+      console.table(data);
+      makeMenu();
+    }
+  });
+}
 
-  viewEmployees() {
-    const getEmployees = `SELECT employee.first_name, employee.last_name, role.title AS Title, department.name AS Department
+const viewEmployees = () => {
+  const getEmployees = `SELECT employee.first_name, employee.last_name, role.title AS Title, department.name AS Department
         FROM employee
         LEFT JOIN role
            ON  employee.role_id = role.id
         LEFT JOIN department
            ON role.department_id = department.id`;
 
-    db.query(getEmployees, (err, data) => {
-      if (err) throw err;
-      else console.table(data);
-    });
-  }
+  db.query(getEmployees, (err, data) => {
+    if (err) throw err;
+    else {
+      console.table(data);
+      makeMenu();
+    }
+  })
 }
 
-    // addRole() {}
